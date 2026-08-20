831e32bfa1106c386a356a68e7f33c824091524e
ftistudio <lollipopsarecoolmj@gmail.com>
2026-08-19 20:37:13 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('offline-cache').then((cache) => {
-      return cache.add('/offline.html');
+      return cache.add('/offline');
     })
   );
 });
@@ -9,7 +9,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => {
-      return caches.match('/offline.html');
+      return caches.match('/offline');
     })
   );
 });
